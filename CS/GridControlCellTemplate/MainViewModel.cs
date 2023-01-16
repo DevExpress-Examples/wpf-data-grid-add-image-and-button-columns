665ee3c122718506df3623124093d80ddd19d1c2
Alexander D <alexandr.demenin@devexpress.com>
2023-01-16 09:57:28 +0400

    Initial commit

---
+++ CS/GridControlCellTemplate/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using GridControlCellTemplate.Model;
+
+namespace GridControlCellTemplate
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<Record> Source { get; } = new ObservableCollection<Record>(Record.GetData(100));
+
+        [Command]
+        public void RemoveRow(Record item) {
+            Source.Remove(item);
+        }
+    }
+}
---
+++ CS/GridControlCellTemplate/Model/Item.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace GridControlCellTemplate.Model
+{
+    public class Record : BindableBase
+    {
+        public string Text {
+            get => GetValue<string>();
+            set => SetValue(value);
+        }
+
+        public bool IsRead {
+            get => GetValue<bool>();
+            set => SetValue(value);
+        }
+
+        public Record(string text) {
+            Text = text;
+        }
+
+        public static IEnumerable<Record> GetData(int quantity) {
+            var sentences = Regex.Split(File.ReadAllText(@"Model/Sentences.txt"), @"(?<=[\.!\?])\s+");
+
+            var gen = new Random(42);
+
+            for (int i = 0; i < quantity; i++) {
+                yield return new Record(sentences[gen.Next(sentences.Length)]);
+            }
+        }
+    }
+}
